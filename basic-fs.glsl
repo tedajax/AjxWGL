1e3aac4b3a6e1b760b40b9ed03e11a5ee6a40390
Ted Dobyns <ted.dobyns@gree.co.jp>
2012-09-14 13:31:33 -0700

    perpixel coloring

--- basic-fs.glsl
+++ basic-fs.glsl
@@ -1,8 +1,18 @@
 precision mediump float;
 
 varying vec4 vVertexColor;
+varying vec4 vVertexPosition;
 
 void main()
 {
-	gl_FragColor = vVertexColor;
+	if (vVertexPosition.y < 0.25)
+		gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0) * vVertexColor;
+	else if (vVertexPosition.y < 0.35)
+		gl_FragColor = vec4(0.95, 0.5, 0.25, 1.0) * vVertexColor;
+	else if (vVertexPosition.y < 0.8)
+		gl_FragColor = vec4(0.0, 0.6, 0.0, 1.0) * vVertexColor;
+	else
+		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0) * vVertexColor;
+
+	//gl_FragColor = vVertexColor;
 }
--- basic-vs.glsl
+++ basic-vs.glsl
@@ -6,20 +6,13 @@ attribute vec3 aVertexPosition;
 attribute vec4 aVertexColor;
 
 varying vec4 vVertexColor;
+varying vec4 vVertexPosition;
 
 void main()
 {
 	vVertexColor = aVertexColor;
+	vVertexPosition = vec4(aVertexPosition, 1.0);
 	gl_Position = uProjection * uView * uWorld * vec4(aVertexPosition, 1.0);
-
-	if (aVertexPosition.y < 0.25)
-		vVertexColor = vec4(0.0, 0.0, 1.0, 1.0);
-	else if (aVertexPosition.y < 0.35)
-		vVertexColor = vec4(0.95, 0.5, 0.25, 1.0);
-	else if (aVertexPosition.y < 0.8)
-		vVertexColor = vec4(0.0, 0.6, 0.0, 1.0);
-	else
-		vVertexColor = vec4(1.0, 1.0, 1.0, 1.0);
 
 	//vVertexColor.r = aVertexPosition.y;
 	//vVertexColor.g = aVertexPosition.y;
